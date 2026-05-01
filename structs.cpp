e392e76ac1f30bcb4171ccdab0a16631d993f242
Esteban <esteban.cs.2004@gmail.com>
2026-04-30 22:56:19 -0400

    feat: estructura base de nodos y claves

---
+++ structs.cpp
@@ -0,0 +1,10 @@
+struct MBR {
+    float x1, x2, y1, y2;   //límites del rectángulo
+    int valor;  //posición del hijo en el archivo, -1 si es hoja
+};
+
+struct Nodo {
+    int k; // cantidad de hijos, varía entre 1 y b
+    MBR mbr[204]; //arreglo de b pares clave-valor
+    int pad; // pad: 12 bytes libres para que la estructura coincida con un bloque
+};
